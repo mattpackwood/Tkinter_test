a30d2de0400acd9948332f3fa759209814204bbb
Matt Packwood <mattpackwood@gmail.com>
2019-12-08 16:22:13 -0500

    Update Tkinter_Wikipedia_2.py

--- Tkinter_Wikipedia_2.py
+++ Tkinter_Wikipedia_2.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
- 2 import tkinter as tk
- 3 
- 4 class Application(tk.Frame):
- 5 
- 6     def __init__(self, master=None):
- 7         super(Application, self).__init__(master)
- 8         self.grid()  
- 9         self.createWidgets()
-10 
-11     def createWidgets(self):
-12         self.mondialLabel = tk.Label(self, text='Hello World')
-13         self.mondialLabel.config(bg="#00ffff")
-14         self.mondialLabel.grid()
-15         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-16         self.quitButton.grid()
-17 
-18 app = Application()
-19 app.master.title('Sample application')
-20 app.mainloop()
+import tkinter as tk
+
+class Application(tk.Frame):
+
+ def __init__(self, master=None):
+  super(Application, self).__init__(master)
+  self.grid()  
+  self.createWidgets()
+
+ def createWidgets(self):
+  self.mondialLabel = tk.Label(self, text='Hello World')
+  self.mondialLabel.config(bg="#00ffff")
+  self.mondialLabel.grid()
+  self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+  self.quitButton.grid()
+
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
