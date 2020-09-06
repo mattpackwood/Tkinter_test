c377c5a1b623539df328100f5bc0b405c2f859a0
Matt Packwood <mattpackwood@gmail.com>
2020-09-06 19:10:51 -0400

    Ran Black only (to may errors in PyLint)

--- Conversion.py
+++ Conversion.py
@@ -1,11 +1,13 @@
 from tkinter import ttk
 
+
 def calculate(*args):
     try:
         value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+        meters.set((0.3048 * value * 10000.0 + 0.5) / 10000.0)
     except ValueError:
         pass
+
 
 root = Tk()
 root.title("Feet to Meters")
@@ -22,15 +24,18 @@ feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(
+    column=3, row=3, sticky=W
+)
 
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()
-root.bind('<Return>', calculate)
+root.bind("<Return>", calculate)
 
 root.mainloop()
--- Temperature_Convertor.py
+++ Temperature_Convertor.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 
+
 def fahrenheit_to_celsius():
     """Convert the value for Fahrenheit to Celsius and insert the
     result into lbl_result.
     """
     fahrenheit = ent_temperature.get()
-    celsius = (5/9) * (float(fahrenheit) - 32)
+    celsius = (5 / 9) * (float(fahrenheit) - 32)
     lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
+
 
 # Set-up the window
 window = tk.Tk()
@@ -26,9 +28,7 @@ lbl_temp.grid(row=0, column=1, sticky="w")
 
 # Create the conversion Button and result display Label
 btn_convert = tk.Button(
-    master=window,
-    text="\N{RIGHTWARDS BLACK ARROW}",
-    command=fahrenheit_to_celsius
+    master=window, text="\N{RIGHTWARDS BLACK ARROW}", command=fahrenheit_to_celsius
 )
 lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
 
